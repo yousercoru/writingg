14ebc1c2e4500840c5859dad56bd9ecf5fa7b303
RMG <RMG@MacBookAirdeRoberto.local>
2020-02-09 20:02:44 +0100

    Endpoint delete-concurso hecho y testeado

---
+++ backend/app/webserver/controllers/concurso/delete-concurso.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const Joi = require("@hapi/joi");
+const mysqlPool = require("../../../database/mysql-pool");
+
+async function validate(payload) {
+  const schema = Joi.object({
+    idconcursos: Joi.string().required()
+  });
+
+  Joi.assert(payload, schema);
+}
+
+async function deleteConcurso(req, res, next) {
+  const { idconcursos } = req.params;
+  //const { userId } = req.claims;
+
+  try {
+    await validate({ idconcursos });
+  } catch (e) {
+    return res.status(400).send(e);
+  }
+
+  let connection;
+  try {
+    connection = await mysqlPool.getConnection();
+    const sqlQuery = `UPDATE concursos
+      SET deleted_at = ?
+      WHERE idconcursos = ?
+        
+        AND deleted_at IS NULL`;
+
+    const deleted_at = new Date()
+      .toISOString()
+      .substring(0, 19)
+      .replace("T", " ");
+    const [deletedStatus] = await connection.execute(sqlQuery, [
+      deleted_at,
+      idconcursos
+    ]);
+    connection.release();
+
+    if (deletedStatus.changedRows !== 1) {
+      return res.status(404).send();
+    }
+
+    return res.status(204).send();
+  } catch (e) {
+    if (connection) {
+      connection.release();
+    }
+
+    return res.status(500).send(e.message);
+  }
+}
+
+module.exports = deleteConcurso;
--- backend/app/webserver/routes/concurso-router.js
+++ backend/app/webserver/routes/concurso-router.js
@@ -2,8 +2,9 @@
 
 const express = require("express");
 
-const checkAccountSession = require('../controllers/account/check-account-session');
-const createConcurso = require('../controllers/concurso/create-concurso');
+const checkAccountSession = require("../controllers/account/check-account-session");
+const createConcurso = require("../controllers/concurso/create-concurso");
+const deleteConcurso = require("../controllers/concurso/delete-concurso");
 const getConcursos = require("../controllers/concurso/get-concursos");
 const getConcurso = require("../controllers/concurso/get-concurso");
 
@@ -12,5 +13,6 @@ const router = express.Router();
 router.post("/concursos", checkAccountSession, createConcurso);
 router.get("/concursos", getConcursos);
 router.get("/concursos/:idconcursos", getConcurso);
+router.delete("/concursos/:idconcursos", checkAccountSession, deleteConcurso);
 
 module.exports = router;

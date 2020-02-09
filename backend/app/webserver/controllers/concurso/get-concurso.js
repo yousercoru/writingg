45c7acf76e496328fcd797291599f291d68ca120
RMG <RMG@MacBookAirdeRoberto.local>
2020-02-09 14:54:40 +0100

    Chequeados endpoints de get-concursos y get-concurso de DV

---
+++ backend/app/webserver/controllers/concurso/get-concurso.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const Joi = require("@hapi/joi");
+const mysqlPool = require("../../../database/mysql-pool");
+
+async function validate(payload) {
+const schema = Joi.object({
+idconcursos: Joi.string().required(),
+idusers: Joi.string().guid({
+version: ["uuidv4"]
+}),
+
+rol: Joi.string()
+});
+
+Joi.assert(payload, schema);
+}
+
+async function getConcurso(req, res, next) {
+const { idconcursos } = req.params;
+//const { idusers } = req.claims;
+
+const payload = {
+idconcursos
+//idusers
+};
+
+try {
+await validate(payload);
+} catch (e) {
+return res.status(400).send(e);
+}
+
+try {
+const connection = await mysqlPool.getConnection();
+const getConcursoQuery = `SELECT * FROM concursos WHERE idconcursos = ?`;
+const [results] = await connection.execute(getConcursoQuery, [idconcursos]);
+connection.release();
+if (results.length < 1) {
+return res.status(404).send();
+}
+
+const [concursoData] = results;
+
+return res.send({
+  data: concursoData
+});
+} catch (e) {
+console.error(e);
+res.status(500).send({
+message: e.message
+});
+}
+}
+
+module.exports = getConcurso;
--- backend/app/webserver/routes/concurso-router.js
+++ backend/app/webserver/routes/concurso-router.js
@@ -3,11 +3,14 @@
 const express = require("express");
 
 const checkAccountSession = require('../controllers/account/check-account-session');
-
 const createConcurso = require('../controllers/concurso/create-concurso');
+const getConcursos = require("../controllers/concurso/get-concursos");
+const getConcurso = require("../controllers/concurso/get-concurso");
 
 const router = express.Router();
 
 router.post("/concursos", checkAccountSession, createConcurso);
+router.get("/concursos", getConcursos);
+router.get("/concursos/:idconcursos", getConcurso);
 
 module.exports = router;

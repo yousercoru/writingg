7854c37f30fc32c8a5f4e7dc785c4b2b80123c3d
Dvitofre <55299304+Dvitofre@users.noreply.github.com>
2020-02-12 17:37:53 +0100

    Create perticipante-router-temp.js

---
+++ backend/app/webserver/routes/perticipante-router-temp.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const express = require("express");
+
+const checkAccountSession = require("../controllers/account/check-account-session");
+const checkEscritorRol = require("../controllers/account/check-escritor-rol");
+const getConcursos = require("../controllers/concurso/get-concursos");
+const getConcurso = require("../controllers/concurso/get-concurso");
+const addParticipanteToConcurso = require("../controllers/participante/add-participante-concurso");
+const deleteParticipanteToConcurso = require("../controllers/participante/delete-participante-concurso");
+
+const router = express.Router();
+
+router.get("/concursos", getConcursos);
+router.get("/concursos/:idconcursos", getConcurso);
+router.post(
+  "/concursos/:idconcursos",
+  checkAccountSession,
+  checkEscritorRol,
+  addParticipanteToConcurso
+);
+router.put(
+  "/concursos/:idconcursos",
+  checkAccountSession,
+  deleteParticipanteToConcurso
+);
+
+module.exports = router;

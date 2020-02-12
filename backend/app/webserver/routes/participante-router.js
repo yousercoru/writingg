01d895e67d26cfb8d200f353655050cb90a145f3
RMG <RMG@MacBookAirdeRoberto.local>
2020-02-12 19:53:49 +0100

    Testeados y validados get-concursantes-concurso, delete-participante-concurso y modificados y validados concurso-router y  participoante-router

--- backend/app/webserver/routes/participante-router.js
+++ backend/app/webserver/routes/participante-router.js
@@ -7,11 +7,23 @@ const checkEscritorRol = require("../controllers/account/check-escritor-rol");
 const getConcursos = require("../controllers/concurso/get-concursos");
 const getConcurso = require("../controllers/concurso/get-concurso");
 const addParticipanteToConcurso = require("../controllers/participante/add-participante-concurso");
+const deleteParticipanteToConcurso = require("../controllers/participante/delete-participante-concurso");
 
 const router = express.Router();
 
 router.get("/concursos", getConcursos);
 router.get("/concursos/:idconcursos", getConcurso);
-router.post("/concursos/:idconcursos", checkAccountSession, checkEscritorRol, addParticipanteToConcurso);
+router.post(
+  "/concursos/:idconcursos",
+  checkAccountSession,
+  checkEscritorRol,
+  addParticipanteToConcurso
+);
+router.put(
+  "/concursos/:idconcursos",
+  checkAccountSession,
+  checkEscritorRol,
+  deleteParticipanteToConcurso
+);
 
 module.exports = router;

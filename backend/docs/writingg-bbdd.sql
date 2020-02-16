25f8ebdeea7782a2ce1eba0d9b97572bc407102f
RMG <RMG@MacBookAirdeRoberto.local>
2020-02-16 13:25:57 +0100

    Revisando también otros endpoints como get-concursos-by-categoria

--- backend/docs/writingg-bbdd.sql
+++ backend/docs/writingg-bbdd.sql
@@ -1,4 +1,6 @@
 USE writingg;
 SELECT * FROM users;
-SELECT * FROM users_has_concursos;
 SELECT * FROM concursos;
+SELECT * FROM users_has_concursos;
+DELETE FROM users_has_concursos WHERE concursos_idconcursos="a02988a3-43ab-496a-9171-1f272476cc8c";
+DELETE FROM users_has_concursos WHERE users_idusers="c9983d4b-42ed-481b-bf64-2d4ed8a4fae8";

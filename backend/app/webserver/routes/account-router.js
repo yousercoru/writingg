d36630b0976bbd00d96cd6f43c8ccb7dfcf7c6d0
Dvitofre <55299304+Dvitofre@users.noreply.github.com>
2020-02-18 12:22:46 +0100

    Subido update-account.js y actualizado routers

--- backend/app/webserver/routes/account-router.js
+++ backend/app/webserver/routes/account-router.js
@@ -1,10 +1,15 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const createAccount = require('../controllers/account/create-account-controller');
+const express = require("express");
+const checkAccountSession = require("../controllers/account/check-account-session");
+
+const createAccount = require("../controllers/account/create-account-controller");
+const updateAccount = require("../controllers/account/update-account");
 
 const router = express.Router();
 
-router.post('/accounts', createAccount);
+router.post("/accounts", createAccount);
+router.put("/accounts/:userId", checkAccountSession, updateAccount);
 
 module.exports = router;
+

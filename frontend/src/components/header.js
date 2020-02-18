066092550153b4b894aff1a83e91b5e907953db6
RMG <RMG@MacBookAirdeRoberto.local>
2020-02-18 11:17:06 +0100

    Update with saved changes

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,27 +1,22 @@
 import React from "react";
 
-export default function Header() {
+export default function Header( { onLogout }) {
   return (
     <header>
         <div className="logo">
         <a href="#">LOGO</a>
         </div>
-
         <nav>
-            <ul>
-                <li>
-                    <a href="#">Dar de alta un concurso</a>
-                </li>
-                <li>
-                    <a href="#">Registrarse</a>
-                </li>
-                <li>
-                    <a href="#">Iniciar sesión</a>
-                </li>
-            </ul>
+            <div>
+                <a href="#">Dar de alta un concurso</a>
+            </div>
+            <div>
+                <a href="#">Registrarse</a>
+            </div>
+            <div>
+                <a href="#">Iniciar sesión</a>
+            </div>
         </nav>
-
-
     </header>
   );
 }
